75be7d92f8236ec471b87f9c3c3c2f16f400cd86
patrick-replogle <patrickr1138@gmail.com>
2020-01-24 18:13:40 -0800

    basic router files are in place and in use on server.js

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const actionRouter = require("./routers/actions-router.js");
+const projectRouter = require("./routers/projects-router.js");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 module.exports = server;
